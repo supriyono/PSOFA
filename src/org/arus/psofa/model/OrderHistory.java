184071c4f951b81672b3d542aa235f226102988d
Supriyono <supri.161189@gmail.com>
2013-07-19 16:19:15 -0700

    Adding some packages and creating unimplemented Account, BillingInfo, Customer and OrderHistory class

---
+++ src/org/arus/psofa/model/OrderHistory.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.arus.psofa.model;
+
+/**
+ *
+ * @author Supri
+ */
+public class OrderHistory {
+    
+}
